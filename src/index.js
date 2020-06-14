d77ad96602207f4caf932b5b7d168ccbfffb5bb1
phelipegomes <61975025+phelipegomes@users.noreply.github.com>
2020-06-13 22:53:28 -0300

    --- Auto-Git Commit ---

--- src/index.js
+++ src/index.js
@@ -5,20 +5,23 @@ import ReactDOM from 'react-dom'
 // import Primeiro from './components/Primeiro'
 // import ComParametro from './components/ComParametro'
 import ComFilhos from './components/ComFilhos'
+import Card from './components/layout/Card'
 
 ReactDOM.render(
     <div>
-        <ComFilhos>
+        <Card titulo = "exercício x">
+            Conteudo
+        </Card>
+        {/* <ComFilhos>
             <ul>
                 <li>mattel</li>
                 <li>phelp</li>
                 <li>genezi</li>
                 <li>durce</li>
             </ul>
-        </ComFilhos>
+        </ComFilhos> */}
         {/* <Primeiro/>
-        <ComParametro titulo= "Esse é o título"
-            subtitulo="Esse é o subtitulo"/>  
+        <ComParametro titulo= "Esse é o título" subtitulo="Esse é o subtitulo"/>  
         <ComParametro titulo= "Opa!" subtitulo="Epa"/> */}     
     </div>,
     document.getElementById('root')
