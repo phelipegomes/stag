81237427ae90c2120192b874f42692ce241d336e
phelipegomes <61975025+phelipegomes@users.noreply.github.com>
2020-06-13 23:23:34 -0300

    --- Auto-Git Commit ---

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import React from "react"
+import './index.css'
+import ReactDOM from 'react-dom'
+
+export default props =>
+
+<div className="App">
+    <Card titulo = "Primeiro Componente">
+        <Primeiro />
+    </Card>
+</div>
+<div>
+    <Card titulo = "exercício x"> Conteudo </Card>   
+</div>
+    document.getElementById('root')
+  
--- src/index.js
+++ src/index.js
@@ -1,28 +1,11 @@
 import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-// import Primeiro from './components/Primeiro'
-// import ComParametro from './components/ComParametro'
-import ComFilhos from './components/ComFilhos'
-import Card from './components/layout/Card'
+import App from './App.jsx'
 
 ReactDOM.render(
-    <div>
-        <Card titulo = "exercício x">
-            Conteudo
-        </Card>
-        {/* <ComFilhos>
-            <ul>
-                <li>mattel</li>
-                <li>phelp</li>
-                <li>genezi</li>
-                <li>durce</li>
-            </ul>
-        </ComFilhos> */}
-        {/* <Primeiro/>
-        <ComParametro titulo= "Esse é o título" subtitulo="Esse é o subtitulo"/>  
-        <ComParametro titulo= "Opa!" subtitulo="Epa"/> */}     
-    </div>,
+    <App>
+    </App>,
+
     document.getElementById('root')
 )
